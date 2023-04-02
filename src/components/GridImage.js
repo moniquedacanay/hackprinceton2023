95a76a5f1a53d27766b14bea2e0fe3faa03e3daf
Shizhan Li <maomaozhuce@163.com>
2023-04-01 23:33:32 -0400

    adding detail page

--- src/components/GridImage.js
+++ src/components/GridImage.js
@@ -1,10 +1,16 @@
 import styles from "./GridImage.module.css";
+import { useRouter } from "next/router";
 const GridImage = ({ recipes }) => {
+  const router = useRouter();
   console.log(recipes);
+  const handleClick=(index)=>{
+    router.push(`/recipe?id=${index}`);
+  }
+
   return (
     <div className={styles.grid}>
       {recipes.map((recipe, index) => (
-        <div key={index}  className={styles.item}>
+        <div key={index}  className={styles.item} onClick={()=>{handleClick(index)}}>
             <img src={recipe.recipe.image} alt="picture" />
           <div className={styles.title}>{recipe.recipe.label}</div>
         </div>
