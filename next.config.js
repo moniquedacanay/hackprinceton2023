8f01afa89744769f47a2fccd913bb077cc4921b3
Andrew Gocheco <arcgoche@uwaterloo.ca>
2023-04-02 11:44:12 -0400

    testing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const { parsed: localEnv } = require('dotenv').config()
+//const { parsed: localEnv } = require('dotenv').config()
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    ...localEnv
-  }
 }
 
 module.exports = nextConfig
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,15 +2,15 @@ import '@/styles/globals.css'
 
 
 export default function App({ Component, pageProps }) {
-  getResponseTest();
+//  getResponseTest();
   return <Component {...pageProps} />
 }
-
+/*
 function getResponseTest() {
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '171e0f370cmshac57882f1db523ep1bff34jsn72922081c5ba',
+      'X-RapidAPI-Key': ,
       'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
     }
   };
@@ -20,3 +20,4 @@ function getResponseTest() {
     .then(response => console.log(response))
     .catch(err => console.error(err));
 }
+*/
