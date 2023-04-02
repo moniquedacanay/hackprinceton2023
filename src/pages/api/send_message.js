abcf93145693f3bfac3673956cd1d1381e5cf202
Shizhan Li <maomaozhuce@163.com>
2023-04-02 11:01:50 -0400

    delete api key

--- src/pages/api/send_message.js
+++ src/pages/api/send_message.js
@@ -1,6 +1,6 @@
 import twilio from 'twilio';
 const accountSid = 'AC1cc8792e79baa76c052a8ea1641cb5de';
-const authToken = '60a930fc878b3cb6f73426e92914bbcc';
+const authToken = '835d2298a7384b0d81c1f94f68def254';
 const client = twilio(accountSid, authToken);
 export default async function handler(req, res) {
     const { phoneNumber, message } = req.body;
