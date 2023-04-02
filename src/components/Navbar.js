0ae5635d6c323c3e71ec216876f706a69bfcf1a6
Shizhan Li <maomaozhuce@163.com>
2023-04-02 00:00:02 -0400

    update the color of the label

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import styles from "./Navbar.module.css"
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-      <div ><img src="/logo.png" style={{width:"60px",height:"60px"}}/></div>
+      <div ><img src="/logo.jpg" style={{width:"80px",height:"80px"}}/></div>
       <div className={styles.name}>Sustain-a-bite</div>
     </nav>
   );
